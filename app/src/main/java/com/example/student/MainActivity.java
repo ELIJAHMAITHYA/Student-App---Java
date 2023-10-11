8111524a98b947eec3915a0416886a16f808ec8d
Sila Elijah <elimaithya21@gmail.com>
2023-10-11 11:45:40 +0300

    Student Details and Methods

---
+++ app/src/main/java/com/example/student/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Integer x, y, z;
+    EditText editText;
+    EditText editText2;
+    Button button;
+    EditText editText3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextText);
+        editText2 = findViewById(R.id.editTextText2);
+        button = findViewById(R.id.button);
+        editText3 = findViewById(R.id.editTextText3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Student student = new Student();
+                student.name = "Sila";
+                student.idno = Integer.parseInt(editText2.getText().toString());
+                student.gender = "Male";
+                editText.setText(student.Display());
+                Toast.makeText(MainActivity.this, "Student Id is" + student.idno.toString(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "The sum  is" + Add(24, 4).toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+    Integer Add(int first, int second) {
+        return (first + second);
+    }
+}
---
+++ app/src/main/java/com/example/student/Student.java
@@ -0,0 +1,18 @@
+package com.example.student;
+
+public class Student {
+    String name;
+    Integer idno;
+    String gender;
+    public String Display(
+
+    ){
+        return "Dekut";
+    }
+    public int calculateNetIncome(int income) {
+        int netIncome = (int) (income * 0.85);
+        return netIncome;
+    }
+
+
+}
